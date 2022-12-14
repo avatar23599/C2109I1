12ed417b345ae82ae48fe8925697cf85abc69f15
Lâm Bùi <avatar23599@gmail.com>
2022-12-14 13:29:58 +0700

    so sánh kiểu số thực

--- 14-12-2022GitHub/CSharpGithub/Program.cs
+++ 14-12-2022GitHub/CSharpGithub/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿//format code Ctrl K +D
+double a = 0.1;
+double b = 0.2;
+Console.WriteLine(a+ b ==0.3?"true":"false");
+//=>false
+
+decimal c = 0.1M;
+decimal d = 0.2M;
+Console.WriteLine(c + d == 0.3M ? "true" : "false");
+//=>true
