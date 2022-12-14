6a852c4308bc680454b1860b4aba4343dfdb654e
Lâm Bùi <avatar23599@gmail.com>
2022-12-14 14:37:35 +0700

    foreach

--- 14-12-2022GitHub/FoeachWhile/Program.cs
+++ 14-12-2022GitHub/FoeachWhile/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System.Collections;
+using System.Text;
+
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+string[] arr = { "Null", "NullLy", "Lâm" };
+//foreach (var item in arr)
+//{
+//    Console.WriteLine($"{item} có chiều dài {item.Length}");
+//}
+
+IEnumerable e = (IEnumerable)arr.GetEnumerator();
+while (e.MoveNext())
+{
+    string s = (string)e.Current;
+    Console.WriteLine($"{s} có chiều dài {s.Length}");
+}
