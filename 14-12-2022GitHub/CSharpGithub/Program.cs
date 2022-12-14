384ec09d5afff9f605805aca88900e3f1dab42ff
Lâm Bùi <avatar23599@gmail.com>
2022-12-14 13:55:04 +0700

    hoàn tatt dau hoi

--- 14-12-2022GitHub/CSharpGithub/Program.cs
+++ 14-12-2022GitHub/CSharpGithub/Program.cs
@@ -8,3 +8,29 @@ decimal c = 0.1M;
 decimal d = 0.2M;
 Console.WriteLine(c + d == 0.3M ? "true" : "false");
 //=>true
+
+//? Nullable value type
+string? fullname = null;
+//int i = null;
+//CSharp cũ
+//Nullable<int> i = null;
+//CSharp mới
+bool? check = null;
+
+//int lenght = fullname != null ? fullname.Length : 0; 
+
+//int? lenght = fullname is not null  ? fullname.Length : null;
+
+//var r = 10;
+//if(r is 10)
+//{
+
+//}
+
+//null-conditional operator ?
+//? thứ 2 tương tự !=, fullname mà khác null thì nó sẽ trả về vế sau.
+//int? lenght = fullname?.Length;
+
+//??-> null-coalescing operator
+//tương tự dấu ==, fullname = null trả về vế sau
+int? lenght = fullname?.Length??0;
