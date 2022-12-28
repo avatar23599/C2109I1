a502dcd68ee00864d58a0670974575d0d6828550
Lâm Bùi <avatar23599@gmail.com>
2022-12-28 14:06:59 +0700

    tuple

--- 14-12-2022GitHub/FuncAndAction/Program.cs
+++ 14-12-2022GitHub/FuncAndAction/Program.cs
@@ -2,6 +2,8 @@
 {
     //local function->hàm cục bộ nằm trong một phương thức nào đó
     public static void Add(int a, int b) => Console.WriteLine(a + b);
+
+    public static int Substract(int a, int b) => a - b;
     public static void Show() => Console.WriteLine("C2109I1");
 
     public static int ReturnNumber() => 5;
@@ -15,10 +17,23 @@
 
         Action act = Show;
         act();
+        //act = Sub;//local function không thể sử dụng delegate
         Func<int> fun = ReturnNumber;
         Console.WriteLine(fun());
 
         Action<int, int> ac = Add;
         ac(4, 6);
+
+        Func<int, int, int> func = Substract;
+        Console.WriteLine(func(10,5));
+
+        Action<string> action = (str) =>
+        {
+            Console.WriteLine($"{str}");
+        };
+
+        action("Tối nay đi uống hồng trà");
+
+        //action.Invoke();
     }
 }
---
+++ 14-12-2022GitHub/Tuple/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using Tuple;
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+
+Person per = new()
+{
+    Id = 1,
+    FirstName = "Tối",
+    LastName = "nay uống hồng trà"
+};
+
+Console.WriteLine(per.Id);
+Console.WriteLine(per.FirstName+per.LastName);
+
+//dùng tuple không cần đến class(struct)
+ValueTuple<int, string, string> p1 = (1, "tối", "nay Huân bao 1 ly hồng trà 35 ngàn");
+Console.WriteLine(p1.Item2+p1.Item3);
+
+(int, string, string) p2 = (1, "Tối nay", "Huân bao một ly hồng trà 35 ngàn");
+Console.WriteLine(p2.Item2+p2.Item3);
+
+var p3 = (1, "Tối nay", "Huân bao một ly hồng trà 35 ngàn");//value tuple
+
+var p4 = (1); // variable int p4 = 1
+
+//net core dùng cái name thay thế cho các Item1, item2,..
+(int Id, string FristName, string LastName) p5 = (1, "Tối nay", "Huân bao một ly hồng trà 35 ngàn");
+Console.WriteLine(p5.FristName + p5.LastName);
+
+var p6 = (id: 1, FristName: "Tối nay",  "Huân bao ly hồng trà 35 ngàn");
+Console.WriteLine(p6.FristName + p6.Item3);
+
+var p7 = new { id = 1, fristname = "Null", lastname = "Ly" };
+Console.WriteLine(p7.fristname+p7.lastname);
